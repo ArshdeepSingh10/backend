5873677b5e21b8cb4230412606038443088b53f3
ArshdeepSingh10 <114170992+ArshdeepSingh10@users.noreply.github.com>
2023-08-15 21:00:22 +0530

    Update index.js

--- index.js
+++ index.js
@@ -98,7 +98,12 @@ app.post('/login', loginuser);
 // app.get('/account', auths);
 app.get('/account', auths , async function(req,res){
     if (req.query.clearToken ==='true') {
-       await res.clearCookie('token');
+       await res.clearCookie('token'{
+        httpOnly: true,
+        secure: true, // Set to true if using HTTPS
+        sameSite: "none",
+        expires: new Date(0),
+       });
         res.status(200).send('Token cookie deleted');
       } else {
         const userData = req.user;
