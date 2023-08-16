de772218968a34aa72600cf73e4bfac1fe9406cb
ArshdeepSingh10 <114170992+ArshdeepSingh10@users.noreply.github.com>
2023-08-16 16:53:40 +0530

    Update index.js

--- index.js
+++ index.js
@@ -115,7 +115,7 @@ app.get('/account', auths , async function(req,res){
       }
 });
 
-app.post("/admin",upload.array("image" ,4), createProduct);
+app.post("/admin", createProduct);
 
 app.get('/:category' , showallproduct);
 app.get('/:category/:proid',showallproduct);
