42eaf19ebf2951a9fee3d68c16673f0fae03858b
ArshdeepSingh10 <114170992+ArshdeepSingh10@users.noreply.github.com>
2023-08-16 19:50:04 +0530

    Update index.js

--- index.js
+++ index.js
@@ -26,12 +26,13 @@ app.use(cors({
 }));
 
 app.use(express.json());
-cloudinary.config(process.env.CLOUDINARY_URL);
-// cloudinary.config({ 
-//     cloud_name: 'dh1bbfjn1', 
-//     api_key: '654517794976445', 
-//     api_secret: 'u2TF9pEUPehBG-EBnPVWthnvzE4' 
-//   });
+// cloudinary.config(process.env.CLOUDINARY_URL);
+cloudinary.config({ 
+    cloud_name: 'dh1bbfjn1', 
+    api_key: '654517794976445', 
+    api_secret: 'u2TF9pEUPehBG-EBnPVWthnvzE4',
+     secure: true,
+  });
 
 
 main().catch((err) => console.log(`unable to connect ${err}`));
