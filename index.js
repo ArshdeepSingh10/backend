9b8601bf42db21594c9144d1f30ca8a41f77e2e6
ArshdeepSingh10 <114170992+ArshdeepSingh10@users.noreply.github.com>
2023-08-16 20:14:14 +0530

    Update index.js

--- index.js
+++ index.js
@@ -93,7 +93,7 @@ const storage = multer.diskStorage({
 //       console.log(err)
 //   }
 //   })
-app.post("/admin", upload.array('image'), createProduct);
+app.post('/admin', upload.array('image'), createProduct);
 app.get('/admin/order',  allorder);
 app.patch('/admin/order' , updateOrder)
 app.get('/checkout',auth)
