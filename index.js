01bced1df95a17e38179a55244db4a3f798de78d
ArshdeepSingh10 <114170992+ArshdeepSingh10@users.noreply.github.com>
2023-08-22 22:01:00 +0530

    Update index.js

--- index.js
+++ index.js
@@ -30,8 +30,7 @@ app.use(express.json());
 cloudinary.config({ 
     cloud_name: 'dh1bbfjn1', 
     api_key: '654517794976445', 
-    api_secret: 'u2TF9pEUPehBG-EBnPVWthnvzE4',
-     secure: true,
+    api_secret: 'u2TF9pEUPehBG-EBnPVWthnvzE4'
   });
 
 
